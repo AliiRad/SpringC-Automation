9e953f2703ae249e2a3655d79077b7c6c770c54c
Satyar Sh <78806874+satyarsh@users.noreply.github.com>
2024-04-05 14:06:59 +0330

    Fixing Imports

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/Model/PersonModel.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/Model/PersonModel.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import com.mft.spring.Enum.Gender;
-import com.mft.spring.Enum.IsMarried;
+import com.home.SpringBootAutomation.Enum.Gender;
+import com.home.SpringBootAutomation.Enum.IsMarried;
 
 @Entity
 @SuperBuilder
