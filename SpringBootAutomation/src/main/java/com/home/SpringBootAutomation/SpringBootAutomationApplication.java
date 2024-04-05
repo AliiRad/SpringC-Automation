10d3be2e3660aed18327a4c65e1cb17c92b713e7
AliRad <alixrad77@gmail.com>
2024-04-05 12:31:33 +0330

    Insert main projct into SpringC-Automation Repo

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/SpringBootAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.home.SpringBootAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutomationApplication.class, args);
+	}
+
+}
---
+++ SpringBootAutomation/src/test/java/com/home/SpringBootAutomation/SpringBootAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.SpringBootAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
