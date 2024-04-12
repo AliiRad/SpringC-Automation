75bebc18c71f9c57a38b0da90bdb8cd60bd137c7
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-12 17:00:09 +0430

    JobsRepository Added - Mahoor

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/JobsRepository.java
@@ -0,0 +1,37 @@
+package com.home.SpringBootAutomation.repository;
+
+import com.home.SpringBootAutomation.Model.Jobs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SuppressWarnings("ALL")
+@Repository
+public interface JobsRepository extends JpaRepository<Jobs, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("update jobsEntity  oo set oo.deleted=true where oo.id=:id")
+    void logicalRemove(Long id);
+
+    List<Jobs> findByCompanyName(String companyName);
+    List<Jobs> findByAddress(String address);
+
+
+//    Jobs findJobsByPersonId(Long id);
+
+
+    //TODO: findJobsBySumOfYears() ----> for finding the years of work experience
+    // i think this method should be written in personRepository to find people with a given years of work experience.
+//    List<Jobs> findJobsByYearOfWorkExperience()
+
+
+
+    //TODO: findAllDeletedFalse()
+    //TODO: findAllDeletedTrue()
+
+}
