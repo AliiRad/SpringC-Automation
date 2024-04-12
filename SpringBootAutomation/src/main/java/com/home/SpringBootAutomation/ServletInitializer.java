0e09b5e9702c8f646f53ef3eefc19625c11d4903
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-12 17:10:11 +0430

    ServletInitializer Added - Mahoor

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.home.SpringBootAutomation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootAutomationApplication.class);
+    }
+}
