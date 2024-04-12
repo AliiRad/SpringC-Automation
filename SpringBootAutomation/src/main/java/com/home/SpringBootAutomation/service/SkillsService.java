559a2a31c30d4ee3e1f68b00cef0fef2612a3fb8
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-12 17:10:49 +0430

    SkillsService Added - Mahoor

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/SkillsService.java
@@ -0,0 +1,20 @@
+package com.home.SpringBootAutomation.service;
+
+import com.home.SpringBootAutomation.Enum.SkillsGradeEn;
+import com.home.SpringBootAutomation.Model.Skills;
+
+import java.util.List;
+
+public interface SkillsService {
+    Skills save (Skills skills);
+    Skills update(Long id , Skills skills);
+    void logicalRemove(Long id);
+    List<Skills> findAll();
+    Skills findById(Long id);
+    List<Skills> findSkillsBySkillTitleContaining(String title);
+    List<Skills> findSkillsByRate(SkillsGradeEn rate);
+    List<Skills> findSkillsByTrainingContaining(String training);
+    //    List<Skills> findSkillsByPersonId(Long id);
+    List<Skills> getSkillsByPagination(int pageNo, int pageSize);
+    Long getSkillsCount();
+}
