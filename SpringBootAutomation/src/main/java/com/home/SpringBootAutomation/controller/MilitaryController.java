58cd92eed5b0492d475dd1388aa6a07ad34c0a6e
farhad <142200730+farhaadghasemi@users.noreply.github.com>
2024-04-14 12:56:04 +0330

    Add files via upload
    Controller-FarhadGhasemi

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/controller/MilitaryController.java
@@ -0,0 +1,92 @@
+package com.home.SpringBootAutomation.controller;
+
+import com.home.SpringBootAutomation.Model.MilitaryServiceModel;
+import com.home.SpringBootAutomation.service.Impl.MilitaryServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "/military")
+public class MilitaryController {
+    private MilitaryServiceImpl militaryService;
+
+    public MilitaryController(MilitaryServiceImpl militaryService) {
+        this.militaryService = militaryService;
+    }
+    @GetMapping
+    public String showMilitary(Model model){
+        log.info("Controller-MilitaryController-Get-FindAll");
+        model.addAttribute("military",new MilitaryServiceModel());
+        model.addAttribute("militaryList",militaryService.findAll());
+        return "military";
+    }
+    @GetMapping(value = "/id{id}")
+    public String showMilitaryFindById(@ModelAttribute("id")Long id){
+        log.info("Controller-MilitaryController-Get-FindById");
+        Optional<MilitaryServiceModel>military=Optional.ofNullable(militaryService.findById(id));
+        if (military.isPresent()){
+            return "military";
+        }else{
+            return "error-404";
+        }
+    }
+    @GetMapping(value = "/serialNumber")
+    public String showMilitaryFindBySerialNumber(Model model,@ModelAttribute("serialNumber")String serialNumber){
+        log.info("Controller-MilitaryController-Get-FindBySerialNumber");
+        List<MilitaryServiceModel> militaryServiceModelList =militaryService.findBySerialNumber(serialNumber);
+        if (!militaryServiceModelList.isEmpty()){
+            model.addAttribute("militaryList", militaryServiceModelList);
+            return "serialNumber";
+        }else {
+            return "error-404";
+        }
+    }
+
+    @GetMapping(value = "/date")
+    public String showMilitaryFindByIssuanceDate(Model model, @ModelAttribute("timeStamp")LocalDateTime issuanceDate){
+        log.info("Controller-MilitaryController-Get-FindByIssuanceDate");
+        List<MilitaryServiceModel> militaryServiceModelList =militaryService.findByIssuanceDate(issuanceDate);
+        if (!militaryServiceModelList.isEmpty()){
+            model.addAttribute("militaryList", militaryServiceModelList);
+            return "military";
+        }else {
+            return "errore-404";
+        }
+    }
+    @PostMapping(value = "/save")
+    public String saveMilitary(MilitaryServiceModel militaryServiceModel){
+        log.info("Controller-MilitaryController-Post-SaveMilitary: " + militaryServiceModel.toString());
+        log.info("Controller-MilitaryController-Post-SaveMilitary");
+        militaryService.save(militaryServiceModel);
+        return "redirect:/MilitaryServiceModel";
+    }
+    @PostMapping(value = "/edit")
+    public String editMilitary(MilitaryServiceModel militaryServiceModel){
+        log.info("Controller-MilitaryController-Post-EditMilitary");
+        militaryService.edit(militaryServiceModel);
+        return "redirect:/MilitaryServiceModel";
+    }
+    @PostMapping(value = "/vitiation")
+    public String militaryVitiation(MilitaryServiceModel militaryServiceModel){
+        log.info("Controller-MilitaryController-Post-militaryVitiation");
+        militaryService.militaryVitiation(militaryServiceModel.getId());
+        return "redirect:/MilitaryServiceModel";
+    }
+    @PostMapping(value = "/delete")
+    public String militaryDeleted(MilitaryServiceModel militaryServiceModel){
+        log.info("Controller-MilitaryController-Post-militaryDeleted");
+        militaryService.militaryVitiation(militaryServiceModel.getId());
+        return "redirect:/military";
+    }
+
+}
---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/controller/PersonController.java
@@ -0,0 +1,85 @@
+package com.home.SpringBootAutomation.controller;
+
+import com.home.SpringBootAutomation.Model.PersonModel;
+import com.home.SpringBootAutomation.service.Impl.PersonServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "/person")
+public class PersonController {
+    private PersonServiceImpl personService;
+
+    public PersonController(PersonServiceImpl personService) {
+        this.personService = personService;
+    }
+    @GetMapping
+    public String showPerson(Model model){
+        log.info("Controller-PersonController-Get-FindAll");
+        model.addAttribute("person",new PersonModel());
+        model.addAttribute("personList",personService.findAll());
+        return "PersonModel";
+    }
+    @GetMapping(value = "/id{id}")
+    public String showPersonFindById(@ModelAttribute("id")Long id){
+        log.info("Controller-PersonController-Get-FindById");
+        Optional<PersonModel>person=Optional.ofNullable(personService.findById(id));
+        if (person.isPresent()){
+            return "PersonModel";
+        }else {
+            return "error-404";
+        }
+    }
+    @GetMapping(value = "/nationalId")
+    public String showPersonFindByNationalId(Model model,@ModelAttribute("nationalId")String nationalId){
+        log.info("Controller-PersonController-Get-FindByNationalId");
+        List<PersonModel> personModelList =personService.findByNationalId(nationalId);
+        if (!personModelList.isEmpty()){
+            model.addAttribute("personList", personModelList);
+            return "nationalId";
+        }else {
+            return "error-404";
+        }
+    }
+    @GetMapping(value = "/date")
+    public String showPersonFindByBirthDate(Model model, @ModelAttribute("timeStamp") LocalDateTime birthdate){
+        log.info("Controller-PersonController-Get-FindByBirthDate");
+        List<PersonModel> personModelList =personService.findByBirthDate(birthdate);
+        if (!personModelList.isEmpty()){
+            model.addAttribute("personList", personModelList);
+            return "person";
+        }else {
+            return "error-404";
+        }
+    }
+    @PostMapping(value = "/save")
+    public String savePerson(PersonModel personModel){
+        log.info("Controller-PersonController-Post-SavePerson"+ personModel.toString());
+        log.info("Controller-PersonController-Post-SavePerson");
+        personService.save(personModel);
+        return "redirect:/PersonModel";
+    }
+    @PostMapping(value = "/edit")
+    public String editPerson(PersonModel personModel){
+        log.info("Controller-PersonController-Post-EditPerson");
+        personService.edit(personModel);
+        return "person";
+    }
+    @PostMapping(value = "/delete")
+    public String personDeleted(PersonModel personModel){
+        log.info("Controller-PersonController-Post-personDeleted");
+        personService.removeById(personModel.getId());
+        return "redirect:/person";
+    }
+
+}
