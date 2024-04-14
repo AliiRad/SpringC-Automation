d0115edb4c6e91862f535ba7be1a42f2d8717456
kurromii <ahadzadeparnian@gmail.com>
2024-04-14 05:09:32 +0330

    salary entity added -parnian

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/Model/Salary.java
@@ -0,0 +1,76 @@
+package com.home.SpringBootAutomation.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity(name = "salaryEntity")
+@Table(name = "salary_tbl")
+public class Salary implements Serializable {
+    //جدول حقوق سالانه
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "salary_seq")
+    @SequenceGenerator(name = "salary_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    //حقوق پایه ساعتی
+    @Column(name = "basic_hourly_pay")
+    private Integer basicHourlyPay;
+
+    //حقوق پایه روزانه
+    @Column(name = "basic_daily_pay")
+    private Integer basicDailyPay;
+
+    //حقوق پایه ماهانه
+    @Column(name = "basic_monthly_pay")
+    private Integer basicMonthlyPay;
+
+    //بن کارگری
+    @Column(name = "working_coupon")
+    private Integer workingCoupon;
+
+    //حق مسکن
+    @Column(name = "housing_right")
+    private Integer housingRight;
+
+    //پایه سنوات
+    @Column(name = "working_year_pay")
+    private Integer workingYearPay;
+
+    //حق اولاد برای هر فرزند
+    @Column(name = "pay_for_each_child")
+    private Integer payForEachChild;
+
+    //حق عائله مندی افراد متاهل
+    @Column(name = "married_people_rights")
+    private Integer marriedPeopleRights;
+
+    //حق بیمه سهم کارگر
+    @Column(name = "insurance")
+    private Integer insurance;
+
+    @Column(name = "year", length = 4, unique = true, nullable = false)
+    @Min(1350)
+    @Max(1490)
+    @NotNull(message = "fill the field")
+    private Integer year;
+
+    @Column(name = "salary_deleted")
+    private Boolean deleted = false;
+
+}
