326d6c45164130cd697edbe8da1f456edd032345
Asus <haniyehghahrmani@gmail.com>
2024-04-15 00:34:32 +0330

    haniyeh updated

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/controller/AppointmentDecreeController.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/controller/AppointmentDecreeController.java
@@ -1,15 +1,22 @@
 package com.home.SpringBootAutomation.controller;
 
 import com.home.SpringBootAutomation.Model.AppointmentDecree;
+import com.home.SpringBootAutomation.Model.Salary;
 import com.home.SpringBootAutomation.service.AppointmentDecreeServiceImp;
+import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
-@RequestMapping(value = "/appointmentDecree")
+@RequestMapping
 public class AppointmentDecreeController {
     private AppointmentDecreeServiceImp serviceImp;
 
@@ -17,39 +24,36 @@ public class AppointmentDecreeController {
         this.serviceImp = serviceImp;
     }
 
-    @GetMapping
-    public String showAppointmentDecree(Model model) {
-        log.info("Controller-AppointmentDecree-Get-FindAll");
-        model.addAttribute("appointmentDecree", new AppointmentDecree());
+    @GetMapping(value = "/appointmentDecree")
+    public String showAppointmentDecreeList(Model model) {
+        model.addAttribute("appointmentDecree",new AppointmentDecree());
         model.addAttribute("appointmentDecreeList", serviceImp.findAll());
         return "appointmentDecree";
     }
 
-    @PostMapping(value = "/save")
+    @PostMapping(value = "/appointmentDecree/save")
     public String saveAppointmentDecree(AppointmentDecree appointmentDecree) {
-        log.info("Controller-AppointmentDecree-Save: " + appointmentDecree.toString());
+            serviceImp.save(appointmentDecree);
+            return "redirect:/appointmentDecree";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String showEditPage(@PathVariable("id") Long id, Model model) {
+        AppointmentDecree appointmentDecree = serviceImp.findById(id);
+        if (appointmentDecree != null) {
+            model.addAttribute("appointmentDecreeEdit", appointmentDecree);
+            return "appointmentDecreeEdit";
+        }
+        return "Invalid Id";
+    }
+
+    @PostMapping(value = "/update/{id}")
+    public String editForm(@PathVariable("id") Long id, @Valid AppointmentDecree appointmentDecree) {
         serviceImp.save(appointmentDecree);
         return "redirect:/appointmentDecree";
     }
 
-    @GetMapping(value ="/id/{id}")
-    public String showAppointmentDecree(@PathVariable("id") Long id , Model model){
-        AppointmentDecree appointmentDecree = serviceImp.findById(id);
-        if (appointmentDecree != null){
-            model.addAttribute("appointmentDecree", appointmentDecree);
-            return "appointmentDecree";
-        }else {
-            return "redirect:/appointmentDecree";
-        }
-    }
-
-    @PostMapping(value ="/edit")
-    public String editAppointmentDecree(AppointmentDecree appointmentDecree) {
-        serviceImp.edit(appointmentDecree);
-        return "appointmentDecree";
-    }
-
-    @PostMapping(value ="/delete")
+    @PostMapping(value = "/delete")
     public String deleteAppointmentDecree(Long id) {
         log.info("Controller-AppointmentDecree-Delete: " + id);
         serviceImp.logicalRemove(id);
