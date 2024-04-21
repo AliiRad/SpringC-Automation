6aaad6a61f83c53c3d58f2ca106ccb48fe8c2f04
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 03:41:34 +0430

    SkillsRepository Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/SkillsRepository.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/SkillsRepository.java
@@ -6,37 +6,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
+import java.util.Optional;
 
-@SuppressWarnings("ALL")
+
 @Repository
 public interface SkillsRepository extends JpaRepository<Skills, Long> {
 
 
-    //TODO: contains the word of the title.
-//  @Query("select oo from skillsEntity  where oo.id =: id and update skillsEntity oo set oo.deleted=true where oo.id=:id " )
-
-
-    //TODO: @Transactional Should be on logicalRemove
-    @Transactional
     @Modifying
-    @Query("update skillsEntity oo set oo.deleted=true where oo.id=:id")
+    @Query("update jobsEntity  oo set oo.deleted=true where oo.id=:id")
     void logicalRemove(Long id);
 
-
-    //TODO: how to make it a  dynamic search?
-    List<Skills> findSkillsBySkillTitleContaining(String title);
-// TODO :   List<Skills> findSkillsBySkillTitleContainingIgnoreCase(String title);
-
-    //TODO : SkillRate is Enum .
-    List<Skills> findSkillsByRate(SkillsGradeEn rate);
+    List<Skills> findSkillsByDeletedFalse();
+    Optional<Skills> findSkillsByIdAndDeletedFalse(Long id);
 
 
-    List<Skills> findSkillsByTrainingContaining(String training);
+    List<Skills> findSkillsBySkillTitleContainingIgnoreCaseAndDeletedFalse(String title);
+    List<Skills> findSkillsByRateAndDeletedFalse(SkillsGradeEn rate);
+    List<Skills> findSkillsByTrainingContainingIgnoreCaseAndDeletedFalse(String training);
 
-//    List<Skills> findSkillsByPersonId(Long id);
+    Long countByDeletedFalse();
+
 
 
 
