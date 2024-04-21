5b1ab19c8973a0f17a556e8a2c410bce482e0a22
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 04:12:48 +0430

    Person Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Person.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Person.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.validator.constraints.UniqueElements;
+
 
 import java.time.LocalDate;
 import java.util.List;
