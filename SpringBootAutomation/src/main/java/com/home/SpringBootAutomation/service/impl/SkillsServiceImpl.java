08a86fdc23d43003345e7db12b3fb714dfe9d923
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 03:48:15 +0430

    SkillsServiceImpl Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/SkillsServiceImpl.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/SkillsServiceImpl.java
@@ -6,32 +6,26 @@ import com.home.SpringBootAutomation.model.Skills;
 import com.home.SpringBootAutomation.repository.SkillsRepository;
 import com.home.SpringBootAutomation.service.SkillsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-//@SuppressWarnings("ALL")
+
 @Service
 public class SkillsServiceImpl implements SkillsService {
 
 
     //------------------------------------------------------
-
-    private SkillsRepository repository;
+    // Constructor Injection
+    private final SkillsRepository repository;
 
     @Autowired
-    public void setRepository(SkillsRepository repository){
+    public SkillsServiceImpl (SkillsRepository repository) {
         this.repository = repository;
     }
 
-    //------------------------------------------------------
-
-//    @Autowired
-//    private SkillsRepository repository;
     //------------------------------------------------------
 
 
@@ -44,29 +38,30 @@ public class SkillsServiceImpl implements SkillsService {
 
     @Override
     public Skills update(Long id, Skills skills) {
-        Optional<Skills> optionalSkills = repository.findById(id);
+        Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(id);
+
         if (optionalSkills.isPresent()){
             Skills oldSkills = optionalSkills.get();
             oldSkills.setSkillTitle(skills.getSkillTitle());
             oldSkills.setRate(skills.getRate());
-            oldSkills.setCertification(skills.getCertification());
-            oldSkills.setDescription(skills.getDescription());
             oldSkills.setTraining(skills.getTraining());
+            oldSkills.setDescription(skills.getDescription());
+            oldSkills.setCertification(skills.getCertification());
+
+
 
             return repository.save(oldSkills);
         }else {
             return null;
         }
-
     }
     //------------------------------------------------------
-
 
     @Transactional
     @Override
     public void logicalRemove(Long id) {
-        Optional<Skills> optional = repository.findById(id);
-        if (optional.isPresent()){
+        Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(id);
+        if (optionalSkills.isPresent()){
             repository.logicalRemove(id);
         }else {
             System.out.println(" Skill not found !");
@@ -74,89 +69,86 @@ public class SkillsServiceImpl implements SkillsService {
 
 
     }
-
     //------------------------------------------------------
 
-    //TODO : Check if list is Empty
 
     @Override
     public List<Skills> findAll() {
         return repository.findAll();
     }
-
     //------------------------------------------------------
+
 
     @Override
     public Skills findById(Long id) {
         Optional<Skills> optional = repository.findById(id);
         return optional.orElse(null);
     }
-
     //------------------------------------------------------
 
-
-    //TODO: Check if List is Empty or not.
-    @Override
-    public List<Skills> findSkillsBySkillTitleContaining(String title) {
-        return repository.findSkillsBySkillTitleContaining(title);
-    }
-    //------------------------------------------------------
-
-
-    //TODO: Check if List is Empty or not.
-    @Override
-    public List<Skills> findSkillsByRate(SkillsGradeEn rate) {
-        return repository.findSkillsByRate(rate);
-    }
-    //------------------------------------------------------
-
-
-    //TODO: Check if List is Empty or not.
-    @Override
-    public List<Skills> findSkillsByTrainingContaining(String training) {
-        return repository.findSkillsByTrainingContaining(training);
-    }
-    //------------------------------------------------------
-
-
-    //TODO: Check if List is Empty or not.
-//    @Override
-//    public List<Skills> findSkillsByPersonId(Long id) {
-//        return repository.findSkillsByPersonId(id);
-//    }
-    //------------------------------------------------------
-
-
-    //TODO: Check if List is Empty or not.
-    @Override
-    public List<Skills> getSkillsByPagination(int pageNo , int pageSize){
-        PageRequest pageRequest =PageRequest.of(pageNo-1 , pageSize);
-        Page<Skills> page = repository.findAll(pageRequest);
-
-        return page.getContent();
-    }
-    //------------------------------------------------------
 
     @Override
-    public Long getSkillsCount(){
+    public Long getSkillsCount() {
         return repository.count();
     }
-
     //------------------------------------------------------
+
+
     @Override
-    public Skills logicalRemoveWithReturn(Long id){
-        Optional<Skills> optionalSkills = repository.findById(id);
+    public Skills logicalRemoveWithReturn(Long id) {
+        Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(id);
         if (optionalSkills.isPresent()){
             Skills oldSkills = optionalSkills.get();
             oldSkills.setDeleted(true);
             return repository.save(oldSkills);
-        }else {
+        } else {
             return null;
         }
-
     }
-
     //------------------------------------------------------
 
+
+    @Override
+    public List<Skills> findSkillsByDeletedFalse() {
+        return repository.findSkillsByDeletedFalse();
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public Optional<Skills> findSkillsByIdAndDeletedFalse(Long id) {
+        Optional<Skills> optional = repository.findSkillsByIdAndDeletedFalse(id);
+        if (optional.isPresent()){
+            return optional;
+        }else return  Optional.empty();
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public List<Skills> findSkillsBySkillTitleContainingIgnoreCaseAndDeletedFalse(String title) {
+        return repository.findSkillsBySkillTitleContainingIgnoreCaseAndDeletedFalse(title);
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public List<Skills> findSkillsByRateAndDeletedFalse(SkillsGradeEn rate) {
+        return repository.findSkillsByRateAndDeletedFalse(rate);
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public List<Skills> findSkillsByTrainingContainingIgnoreCaseAndDeletedFalse(String training) {
+        return repository.findSkillsByTrainingContainingIgnoreCaseAndDeletedFalse(training);
+    }
+    //------------------------------------------------------
+
+    @Override
+    public Long countByDeletedFalse() {
+        return repository.countByDeletedFalse();
+    }
+    //------------------------------------------------------
 
 }
