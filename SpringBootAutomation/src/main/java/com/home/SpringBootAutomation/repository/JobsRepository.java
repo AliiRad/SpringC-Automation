60acf41c98ce1f1cfdb30c511a7a7e3373dbbd4e
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 03:40:15 +0430

    JobsRepository Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/JobsRepository.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/JobsRepository.java
@@ -5,33 +5,48 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
+import java.util.Optional;
 
-//@SuppressWarnings("ALL")
 @Repository
 public interface JobsRepository extends JpaRepository<Jobs, Long> {
 
     @Modifying
-    @Transactional
     @Query("update jobsEntity  oo set oo.deleted=true where oo.id=:id")
     void logicalRemove(Long id);
 
-    List<Jobs> findByCompanyName(String companyName);
-    List<Jobs> findByAddress(String address);
+
+    //fetches all jobs that there deleted field is false.
+    List<Jobs> findJobsByDeletedFalse();
+    Optional<Jobs> findJobsByIdAndDeletedFalse(Long id);
 
 
-//    Jobs findJobsByPersonId(Long id);
+    List<Jobs> findJobsByCompanyNameAndDeletedFalse(String companyName);
+    List<Jobs> findJobsByCompanyNameContainingIgnoreCaseAndDeletedFalse(String companyName);
+    List<Jobs> findJobsByAddressAndDeletedFalse(String address);
+
+    Long countByDeletedFalse();
+
+
+
+
+
+
+
+
+
+
 
 
     //TODO: findJobsBySumOfYears() ----> for finding the years of work experience
     // i think this method should be written in personRepository to find people with a given years of work experience.
-//    List<Jobs> findJobsByYearOfWorkExperience()
+    //List<Jobs> findJobsByYearOfWorkExperience()
+    //Jobs findJobsByPersonId(Long id);
 
 
 
-    //TODO: findAllDeletedFalse()
-    //TODO: findAllDeletedTrue()
+
 
 }
