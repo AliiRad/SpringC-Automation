54b7e5cabe157e341ea2117faeee5e35ef22ee1a
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 03:27:55 +0430

    Person Entity Changed Nightly - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Person.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Person.java
@@ -1,89 +1,127 @@
 package com.home.SpringBootAutomation.model;
 
-import com.home.SpringBootAutomation.Enum.Gender;
-import com.home.SpringBootAutomation.Enum.IsMarried;
+
+import com.home.SpringBootAutomation.enums.GenderEn;
+import com.home.SpringBootAutomation.enums.MarriageEn;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
+import java.util.List;
 
-
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
-@SuperBuilder
-@ToString
 
-
-@Entity(name = "PersonEntity")
-@Table(name = "Person_tbl")
+@Entity(name = "personEntity")
+@Table(name = "person_tbl")
 
 
 public class Person {
-
-	//شناس نامه
-
 	@Id
-	@SequenceGenerator(name = "personSeq", sequenceName = "person_seq" , initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SequenceGenerator(name = "personSeq", sequenceName = "person_seq", initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "personSeq")
+	@Column(name = "person_id", nullable = false ,unique = true)
+	private Long id;
 
-	@Column(name = "person_id")
-	private long id;
 
-	@Column(name ="person_userName")
-	private String userName;
-
-	@Column(name ="person_password")
-	private String password;
-
-//	@Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid Name")
-	@Column(name ="person_name")
+	@Column(name ="person_name", length = 50 , nullable = false , columnDefinition = "NVARCHAR2(50)")
+	@Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Name")
+	@Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+	@NotBlank(message = "Should Not Be Null")
 	private String name;
 
-//	@Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid Last Name")
-	@Column(name ="person_lastname")
+	@Column(name ="person_lastname", length = 50 , nullable = false , columnDefinition = "NVARCHAR2(50)")
+	@Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Family")
+	@Size(min = 3, max = 50, message = "Last Name must be between 3 and 50 characters")
+	@NotBlank(message = "Should Not Be Null")
 	private String lastname;
 
-//	@Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid CertificateID")
-	@Column(name ="person_certificateID")
-	private String certificateID; //شماره شناس نامه
+
+	@Column(name ="person_userName" ,length = 30 , nullable = false , columnDefinition = "VARCHAR2(30)")
+	@Pattern(regexp = "^[a-zA-Z\\s]{3,30}$", message = "Invalid Username")
+	@Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+	@NotBlank(message = "Should Not Be Null")
+	private String userName;
+
+	@Column(name ="person_password",length = 30 , nullable = false , columnDefinition = "VARCHAR2(30)")
+	@Pattern(regexp = "^[a-zA-Z\\s]{8,30}$", message = "Invalid Password")
+	@Size(min = 8, max = 30, message = "Password must be between 8 and 30 characters")
+	@NotBlank(message = "Should Not Be Null")
+	private String password;
 
 
-//	@Pattern(regexp = "^[0-9\\s]{10}$", message = "Invalid NationalID")
-	@Column(name = "person_nationalID")
-	private String nationalId; // شماره ملی
+	//TODO: What is certificateID length? Does it Take Characters? If it Dose not take  characters, then the type should be Number?
+	@Column(name ="person_certificateID",length = 12 , nullable = false , columnDefinition = "VARCHAR2(12)")
+	@Pattern(regexp = "^[0-9]{8,12}$", message = "Invalid Certificate ID")
+	@Size(min = 8, max = 12, message = " Certificate ID must be between 8 and 12 characters")
+	@NotBlank(message = "Should Not Be Null")
+	private String certificateID;
 
-//	@PastOrPresent(message = "Invalid birthDate ")
-	@Column(name ="person_birthdate")
+
+	@Column(name = "person_nationalID",length = 12 , nullable = false , columnDefinition = "VARCHAR2(12)")
+	@Pattern(regexp = "^[0-9]{8,12}$", message = "Invalid National ID")
+	@Size(min = 8, max = 12, message = " National ID must be between 8 and 12 characters")
+	@NotBlank(message = "Should Not Be Null")
+	private String nationalId;
+
+
+	@Column(name ="person_birthdate" , nullable = false)
+	@Past(message = "Invalid Birth Date")
+	@NotBlank(message = "Should Not Be Null")
 	private LocalDate birthdate;
 
-//	@Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid City")
-	@Column(name ="person_city")
-	private String city; // شهر
 
-//	@Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid province")
-	@Column(name ="person_province")
-	private String province; //شهرستان
+	@Column(name ="person_city", length = 50 , nullable = false , columnDefinition = "NVARCHAR2(50)")
+	@Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid City Name")
+	@Size(min = 3, max = 50, message = "City Name must be between 3 and 50 characters")
+	@NotBlank(message = "Should Not Be Null")
+	private String city;
 
-	@Column(name ="person_gender")
-	private Gender gender; // Enum or String?
+	@Column(name ="person_province", length = 50 , nullable = false , columnDefinition = "NVARCHAR2(50)")
+	@Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Province Name")
+	@Size(min = 3, max = 50, message = "Province Name must be between 3 and 50 characters")
+	@NotBlank(message = "Should Not Be Null")
+	private String province;
 
-	@Column(name ="person_isMarried")
-	private IsMarried isMarried; //Enum
+	@Column(name ="person_gender", nullable = false)
+	@NotNull(message = "Should Not Be Null")
+	@Enumerated(EnumType.ORDINAL)
+	private GenderEn gender;
 
-//	@Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid FathersName")
-	@Column(name ="person_fathersName")
+
+	@Column(name ="person_marriageStatus", nullable = false)
+	@NotNull(message = "Should Not Be Null")
+	@Enumerated(EnumType.ORDINAL)
+	private MarriageEn marriageStatus;
+
+	@Column(name ="person_fathersName" ,length = 50 , nullable = false , columnDefinition = "NVARCHAR2(50)")
+	@Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Father Name")
+	@Size(min = 3, max = 50, message = "Father Name must be between 3 and 50 characters")
+	@NotBlank(message = "Should Not Be Null")
 	private String fathersName;
 
-	@Column(name ="person_status")
-	private boolean status;//زنده(False) یا مرده(True)؟
+	@Column(name = "person_deleted")
+	private Boolean deleted = false;
 
-	@Column(name ="person_dead")
-	private boolean deleted;//زنده یا مرده؟
+
+	//Jobs Relationship
+//	@OneToMany(cascade = CascadeType.ALL , fetch = FetchType.LAZY, mappedBy = "person")
+//	private List<Jobs> jobs;
+//
+//
+//	//Skills Relationship
+//	@OneToMany(cascade = CascadeType.ALL , fetch = FetchType.LAZY , mappedBy = "person")
+//	private List<Skills> skills ;
+
+
 
 
 
