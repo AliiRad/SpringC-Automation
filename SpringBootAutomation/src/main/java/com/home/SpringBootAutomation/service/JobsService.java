3cbdcee8acd29a342d8475e62722e5c1e0e203d3
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 03:43:39 +0430

    JobsService Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/JobsService.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/JobsService.java
@@ -1,20 +1,38 @@
 package com.home.SpringBootAutomation.service;
 
 import com.home.SpringBootAutomation.model.Jobs;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JobsService {
-    Jobs save(Jobs jobs) ;
-    Jobs update(Long id , Jobs jobs) ;
+
+    Jobs save(Jobs jobs);
+    Jobs update(Long id , Jobs jobs);
+
+    @Transactional
     void logicalRemove(Long id);
+
     List<Jobs> findAll();
     Jobs findById(Long id);
-    List<Jobs> findByCompanyName(String companyName);
-    List<Jobs> findByAddress(String address);
-    //    Jobs findJobsByPersonId(Long id);
-    List<Jobs> getJobsByPagination(int pageNo, int pageSize);
     Long getJobsCount();
+
+
     Jobs logicalRemoveWithReturn(Long id);
+
+
+    //-------------------------------------------------------------------
+    //deletedFalse
+
+    List<Jobs> findJobsByDeletedFalse();
+    Optional<Jobs> findJobsByIdAndDeletedFalse(Long id);
+
+
+    List<Jobs> findJobsByCompanyNameAAndDeletedFalse(String companyName);
+    List<Jobs> findJobsByCompanyNameContainingIgnoreCaseAndDeletedFalse(String companyName);
+    List<Jobs> findJobsByAddressAndDeletedFalse(String address);
+
+    Long countByDeletedFalse();
 
 }
