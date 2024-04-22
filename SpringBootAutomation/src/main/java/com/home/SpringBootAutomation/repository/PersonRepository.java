37a7963d6cb336a05d9c49e3d78f58ae7a85e893
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 04:56:14 +0430

    PersonRepository Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/PersonRepository.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/repository/PersonRepository.java
@@ -1,58 +1,33 @@
 package com.home.SpringBootAutomation.repository;
 
+
 import com.home.SpringBootAutomation.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-
+@Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {
 
     @Modifying
-    @Query("update PersonEntity oo set oo.deleted=true where oo.id=:id")
-    void PersonDeleted(Long id);
+    @Query("update personEntity  oo set oo.deleted=true where oo.id=:id")
+    void logicalRemove(Long id);
 
-    @Query("SELECT oo from PersonEntity oo where oo.nationalId=:nationalId")
-    List<Person> findByNationalID(String nationalId);
+    //fetches all active persons .
+    List<Person> findPersonByDeletedFalse();
+    Optional<Person> findPersonByIdAndDeletedFalse(Long id);
 
-    @Query("SELECT oo from PersonEntity oo where oo.birthdate=:birthdate")
-    List<Person>findByBirthDate(LocalDate birthdate);
-
-    @Query("SELECT oo from PersonEntity oo where oo.name=:name")
-    List<Person> findByName(String name);
-
-    @Query("select oo from  PersonEntity oo where oo.name=:name and oo.lastname=:lastname")
-    List<Person> findByNameAndLastname(String name , String lastname);
-
-    List<Person> findByLastname(String lastname);
-
-    List<Person>findByUserName(String username);
-
-    List<Person>findByCertificateID(String certificateId);
-
-    @Query("select oo from  PersonEntity oo where oo.city=:city and oo.province=:province")
-    List<Person>findByCityAndProvince(String city,String province);
-
-    @Query("select oo from PersonEntity oo where oo.status=:true")
-    List<Person>findByTrueStatus(boolean status);
-
-    @Query("select oo from PersonEntity oo where oo.deleted=:true")
-    List<Person>findByDeleted(boolean deleted);
-
-    @Query("select p.name from PersonEntity  p where p.id  in :id")
-    List<String> findAllNamesById(@Param("id") ArrayList<Long> id);
+    List<Person> findPersonByNameAndLastnameAndDeletedFalse(String name , String lastName);
+    Optional<Person> findPersonByNationalIdAndDeletedFalse(String nationalId);
+    Optional<Person> findPersonByUserNameAndDeletedFalse(String UserName);
+    List<Person> findPersonByCityAndProvinceAndDeletedFalse(String city , String province );
 
 
-//    todo:in 2 query chek shavad;
-    @Query("select oo.status from PersonEntity oo where oo.status=:true")
-    List<Person>findAllUserNameStatusTrue(String name);
 
-    @Query("select oo.deleted from PersonEntity oo where oo.deleted=:true")
-    List<Person>findAllUserNameDeletedTrue(String username);
+    Long countByDeletedFalse();
+
 }
