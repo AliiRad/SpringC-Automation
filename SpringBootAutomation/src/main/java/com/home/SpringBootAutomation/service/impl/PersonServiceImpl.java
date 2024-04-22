eb85aaf7cc60c94df3b4e151ba0ebb08acc6e254
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-22 04:58:57 +0430

    PersonServiceImpl Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/PersonServiceImpl.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/PersonServiceImpl.java
@@ -4,6 +4,7 @@ import com.home.SpringBootAutomation.model.Person;
 import com.home.SpringBootAutomation.repository.PersonRepository;
 import com.home.SpringBootAutomation.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -12,88 +13,140 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Slf4j
 public class PersonServiceImpl implements PersonService {
-    private final PersonRepository personRepository;
 
-    public PersonServiceImpl(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    //------------------------------------------------------
+    // Constructor Injection
+    private final PersonRepository repository;
+
+    @Autowired
+    public PersonServiceImpl (PersonRepository repository) {
+        this.repository = repository;
     }
 
-    @Override
-    public Person save(Person person){
-        log.info("Service-Person-Save");
-        personRepository.save(person);
-        return person;
-    }
+    //------------------------------------------------------
 
     @Override
-    public Person edit(Person person){
-        log.info("Service-Person-Edit");
-        if (findById(person.getId())!=null){
-            personRepository.save(person);
-            return person;
+    public Person save(Person person) {
+        return repository.save(person);
+    }
+
+    //------------------------------------------------------
+
+    @Override
+    public Person update(Long id,Person person) {
+        Optional<Person> optionalPerson = repository.findPersonByIdAndDeletedFalse(id);
+
+        if (optionalPerson.isPresent()){
+            return repository.save(person);
+
+        }else return null;
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public void logicalRemove(Long id) {
+        Optional<Person> optionalPerson = repository.findPersonByIdAndDeletedFalse(id);
+        if (optionalPerson.isPresent()){
+            repository.logicalRemove(id);
+        }else {
+            System.out.println(" Person not found !");
         }
-        else return null;
-    }
 
-    @Override
-    public Person remove(Person person) {
-        log.info("Service-Person-Remove");
-        if (findById(person.getId())!=null){
-            personRepository.delete(person);
-            return person;
-        }
-        else return null;
     }
-    @Override
-    public Person removeById(Long id){
-        log.info("Service-Person-removeById");
-        Person person =findById(id);
-        if (person !=null){
-            person.setDeleted(true);
-            return person;
-        }
-        else return null;
-    }
-
-    @Override
-    public List<Person> findAll(){
-        log.info("Service-Person-FindAll");
-        List<Person>personList=personRepository.findAll();
-        return personList;
-    }
+    //------------------------------------------------------
 
 
     @Override
-    public Person findById(Long id){
-        log.info("Service-Person-FindById");
-        Optional<Person> person=personRepository.findById(id);
-        return(person.orElse(null)) ;
+    public List<Person> findAll() {
+        return repository.findAll();
     }
+    //------------------------------------------------------
+
 
     @Override
-    public List<Person> findByNationalId(String nationalId) {
-        log.info("Service-Person-findByNationalId");
-        return personRepository.findByNationalID(nationalId);
+    public Person findById(Long id) {
+        Optional<Person> optional = repository.findById(id);
+        return optional.orElse(null);
     }
+    //------------------------------------------------------
+
 
     @Override
-    public Person findByName(String name) {
-        log.info("Service-Person-findByName");
-        return (Person) personRepository.findByName(name);
+    public Long getPersonsCount() {
+        return repository.count();
     }
+    //------------------------------------------------------
+
 
     @Override
-    public Person findByNameAndLastname(String name, String lastname) {
-        log.info("Service-Person-findByNameAndLastname");
+    public Person logicalRemoveWithReturn(Long id) {
+        Optional<Person> optionalPerson = repository.findPersonByIdAndDeletedFalse(id);
 
-        return null;
+        if (optionalPerson.isPresent()){
+            Person oldPerson = optionalPerson.get();
+            oldPerson.setDeleted(true);
+            return repository.save(oldPerson);
+
+        }else return null;
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public List<Person> findPersonByDeletedFalse() {
+        return repository.findPersonByDeletedFalse();
     }
 
-    public List<Person>findByBirthDate(LocalDate birthdate){
-        log.info("Service-Person-findByBirthDate");
-        return personRepository.findByBirthDate(birthdate);
+    //------------------------------------------------------
+
+    @Override
+    public Optional<Person> findPersonByIdAndDeletedFalse(Long id) {
+        Optional<Person> optional = repository.findPersonByIdAndDeletedFalse(id);
+        if (optional.isPresent()){return optional;
+        }else return  Optional.empty();
     }
+    //------------------------------------------------------
+
+
+    @Override
+    public List<Person> findPersonByNameAndLastnameAndDeletedFalse(String name, String lastName) {
+        return repository.findPersonByNameAndLastnameAndDeletedFalse(name , lastName);
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public Optional<Person> findPersonByNationalIdAndDeletedFalse(String nationalId) {
+        Optional<Person> optional = repository.findPersonByNationalIdAndDeletedFalse(nationalId);
+        if (optional.isPresent()){return optional;
+        }else return  Optional.empty();
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public Optional<Person> findPersonByUserNameAndDeletedFalse(String UserName) {
+        Optional<Person> optional = repository.findPersonByUserNameAndDeletedFalse(UserName);
+        if (optional.isPresent()){return optional;
+        }else return  Optional.empty();
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public List<Person> findPersonByCityAndProvinceAndDeletedFalse(String city, String province) {
+        return repository.findPersonByCityAndProvinceAndDeletedFalse(city , province);
+    }
+    //------------------------------------------------------
+
+
+    @Override
+    public Long countByDeletedFalse() {
+        return repository.countByDeletedFalse();
+    }
+    //------------------------------------------------------
+
 
 }
