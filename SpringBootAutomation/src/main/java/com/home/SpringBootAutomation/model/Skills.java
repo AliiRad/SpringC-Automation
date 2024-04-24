19f1cd42981408c00d3d64883d87318564c382d9
Satyar Sh <78806874+satyarsh@users.noreply.github.com>
2024-04-24 19:26:53 +0330

    Fixed Import Skills.java

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Skills.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Skills.java
@@ -1,6 +1,6 @@
 package com.home.SpringBootAutomation.model;
 
-import com.home.SpringBootAutomation.Enum.SkillsGradeEn;
+import com.home.SpringBootAutomation.enums.SkillsGradeEn;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
