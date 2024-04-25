ec8fd3f9029726109589ee2ff3d21c9d5c68819e
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-25 21:44:25 +0430

    AppointmentDecree Entity Cleaned - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/AppointmentDecree.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/AppointmentDecree.java
@@ -21,89 +21,90 @@ import java.time.LocalDate;
 @Setter
 //حکم کارگزینی
 
-@Table(name = "appointment_Decree_tbl")
+
 @Entity(name = "appointmentDecreeEntity")
+@Table(name = "appointment_decree_tbl")
 public class AppointmentDecree {
     @Id
     @SequenceGenerator(name = "appointmentDecreeSeq", sequenceName = "appointmentDecree_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "appointmentDecreeSeq")
-    @Column(name = "appointmentDecree_id")
+    @Column(name = "appointment_decree_id")
     private Long id;
 
 //    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
 //    private Person person;
 
-    @Column(name = "appointmentDecree_typeOfEmployment" )
+    @Column(name = "appointment_decree_type_of_employment" )
     @NotBlank(message = "Should Not Be Null")
     @Enumerated(EnumType.ORDINAL)
     private TypeOfEmployment typeOfEmployment;                                                      //نوع استخدام
 
-    @Column(name = "appointmentDecree_organizationalUnit", columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_organizational_unit", columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Organizational Unit")
     @Size(min = 3, max = 20, message = "Organizational Unit must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String organizationalUnit;                                                       //واحد سازمانی
 
-    @Column(name = "appointmentDecree_jobType", columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_job_type", columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Job Type")
     @Size(min = 3, max = 20, message = "Job Type must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String jobType;                                                                  //نوع کار--کارشناس
 
-    @Column(name = "appointmentDecree_professionalField",columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_professional_field",columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Professional Field")
     @Size(min = 3, max = 20, message = "Professional Field must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String professionalField;                                                        //رسته شغلی--زمینه حرفه
 
-    @Column(name = "appointmentDecree_jobCode", columnDefinition = "NVARCHAR2(6)")
+    @Column(name = "appointment_decree_job_code", columnDefinition = "NVARCHAR2(6)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{6}$", message = "Invalid Job Code")
     @Size(min = 6, max = 6, message = "Invalid Job must be 6 characters")
     @NotBlank(message = "Should Not Be Null")
     private String jobCode;                                                                     //کد شغل
 
-    @Column(name = "appointmentDecree_jobTitle",  columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_job_title",  columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Job Title")
     @Size(min = 3, max = 20, message = "Job Title must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String jobTitle;                                                                 //عنوان شغل
 
-    @Column(name = "appointmentDecree_startDate")
+    @Column(name = "appointment_decree_start_date")
     @PastOrPresent(message = "Invalid Start Date")
     private LocalDate startDate;                                                            //تاریخ شروع
 
-    @Column(name = "appointmentDecree_endDate")
+    @Column(name = "appointment_decree_end_date")
     @FutureOrPresent(message = "Invalid End Date")
     private LocalDate endDate;                                                              //تاریخ پایان
 
-    @Column(name = "appointmentDecree_placeOfEmployment",  columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_place_of_employment",  columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Place Of Employment")
     @Size(min = 3, max = 20, message = "Place Of Employment must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String placeOfEmployment;                                                        //محل استخدام
 
-    @Column(name = "appointmentDecree_workingHours", columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_working_hours", columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Working Hours")
     @Size(min = 3, max = 20, message = "Working Hours must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String workingHours;                                                             //ساعت کاری
 
-    @Column(name = "appointmentDecree_workingDay", length = 4)
+    @Column(name = "appointment_decree_working_day", length = 4)
     @Min(1)
     @Max(4)
     private Integer workingDay;                                                                  //روز کارکرد
 
-    @Column(name = "appointmentDecree_appointmentType")
+    @Column(name = "appointment_decree_appointment_type")
     @NotBlank(message = "Should Not Be Null")
     @Enumerated(EnumType.ORDINAL)
     private AppointmentType appointmentType;                                                         //نوع حکم
 
-    @Column(name = "appointmentDecree_appointmentDescription", columnDefinition = "NVARCHAR2(20)")
+    @Column(name = "appointment_decree_appointment_description", columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,20}$", message = "Invalid Appointment Description")
     @Size(min = 3, max = 20, message = "Appointment Description must be between 3 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String appointmentDescription;                                                   //شرح حکم
 
-    @Column(name = "appointmentDecree_deleted")
+    @Column(name = "appointment_decree_deleted")
     private boolean deleted;
 }
