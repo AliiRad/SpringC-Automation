6f48df442462aebddf41b56ef8ff8bea92f66f93
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-25 21:47:44 +0430

    BankTransaction Entity Cleaned - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/BankTransaction.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/BankTransaction.java
@@ -15,27 +15,27 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Setter
 
-@Entity(name = "BankTransactionEntity")
-@Table(name = "BankTransaction_tbl")
+@Entity(name = "bankTransactionEntity")
+@Table(name = "bank_transaction_tbl")
 public class BankTransaction {
     @Id
     @SequenceGenerator(name = "bankTransactionSeq", sequenceName = "bankTransaction_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bankTransactionSeq")
-    @Column(name = "bankTransaction_id",length = 20)
+    @Column(name = "bank_transaction_id",length = 20)
     private Long id;
 
     @ManyToOne(cascade = CascadeType.ALL , fetch = FetchType.LAZY )
-    @JoinColumn(name = "bankTransaction_balance")
+    @JoinColumn(name = "bank_transaction_balance")
     private FinancialDocument balance;
 
     @ManyToOne(cascade = CascadeType.ALL , fetch = FetchType.LAZY )
-    @JoinColumn(name = "bankTransaction_financial_document")
+    @JoinColumn(name = "bank_transaction_financial_document")
     private FinancialDocument financialDocument;
 
     @ManyToOne(cascade = CascadeType.ALL , fetch = FetchType.LAZY )
-    @JoinColumn(name = "bankTransaction_employee")
+    @JoinColumn(name = "bank_transaction_employee")
     private Person employee;
 
-    @Column(name = "bankTransaction_deleted")
+    @Column(name = "bank_transaction_deleted")
     private boolean deleted;
 }
