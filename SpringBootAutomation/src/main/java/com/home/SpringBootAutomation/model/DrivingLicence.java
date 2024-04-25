07c482ce9beca5403f1a063023cd01fe061d2f80
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-25 21:38:33 +0430

    DrivingLicence Entity Cleaned - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/DrivingLicence.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/DrivingLicence.java
@@ -3,16 +3,18 @@ package com.home.SpringBootAutomation.model;
 import com.home.SpringBootAutomation.enums.TypeOfCertification;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import java.time.LocalDate;
 
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
-@SuperBuilder
 
 
 @Table(name = "driving_licence_tbl")
@@ -21,45 +23,49 @@ public class DrivingLicence {
 //	//گواهینامه رانندگی
 
     @Id
-    @SequenceGenerator(name = "drivingLicenceSeq", sequenceName = "drivingLicence_seq", initialValue = 1, allocationSize = 1)
+    @SequenceGenerator(name = "drivingLicenceSeq", sequenceName = "drivingLicence_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "drivingLicenceSeq")
     @Column(name = "driving_licence_id")
     private Long id;
 
-    @Column(name = "drivingLicence_typeOfCertification", nullable = false)
+    @Column(name = "driving_licence_type_of_cert")
     @NotBlank(message = "Should Not Be Null")
     @Enumerated(EnumType.ORDINAL)
     private TypeOfCertification typeOfCertification; //نوع گواهی نامه
 
+    @Column(name = "driving_licence_issuance_date")
     @PastOrPresent(message = "Invalid issuance Date")
-    @Column(name = "drivingLicence_issuanceDate", nullable = false)
     private LocalDate issuanceDate;//تاریخ صدور
 
+
+    @Column(name = "driving_licence_serial_number", length = 20)
     @Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid SerialNumber")
     @Size(min = 1, max = 20, message = "SerialNumber must be between 1 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
-    @Column(name = "drivingLicence_serialNumber", length = 20, nullable = false)
     private String serialNumber; // شماره گواهی نامه
 
-    @Column(name = "drivingLicence_LicenseSuspension", length = 30)
+    @Column(name = "driving_licence_license_suspension", length = 30)
     private boolean licenseSuspension;//تعلیق گواهینامه
 
-    @PastOrPresent(message = "Invalid LicenseSuspension Date")
-    @Column(name = "drivingLicence_LicenseSuspensionDate")
+
+    @Column(name = "driving_licence_license_suspension_date")
+    @PastOrPresent(message = "Invalid License Suspension Date") //TODO: Only @Past !
     private LocalDate licenseSuspensionDate;//تاریخ تعلیق گواهینامه
 
+
+    @Column(name = "driving_licence_end_date")
     @FutureOrPresent(message = "Invalid End Date")
-    @Column(name = "drivingLicence_endDate", nullable = false)
     private LocalDate endDate;//تاریخ پایان اعتبار
 
+
+    @Column(name = "driving_licence_renewal")
     @PastOrPresent(message = "Invalid Renewal Date")
-    @Column(name = "drivingLicence_renewal")
     private LocalDate renewal;//تاریخ تمدید مجدد
 
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "driving_licence_person_id")
     private Person person;
 
-    @Column(name = "drivingLicence_Deleted", length = 30)
+    @Column(name = "driving_licence_deleted")
     private boolean deleted;//حذف
 }
