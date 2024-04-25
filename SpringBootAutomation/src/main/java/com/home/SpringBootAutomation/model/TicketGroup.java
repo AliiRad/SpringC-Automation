740277b009069cfc06afdfec7a07b6eb18217ec2
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-25 22:38:35 +0430

    TicketGroup Refactor And TODO - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/TicketGroup.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/TicketGroup.java
@@ -1,24 +1,37 @@
 package com.home.SpringBootAutomation.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
+
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 
 @Entity(name = "groupEntity")
 @Table(name = "group_tbl")
 public class TicketGroup {
+    //TODO: GenerationType.SEQUENCE
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "g_title")
+    @Column(name = "group_title") //TODO: Put All Needed Validation Constraints .
     private String title;
 
     @ManyToOne
-    @JoinColumn(name = "g_parent_id")
+    @JoinColumn(name = "group_parent_id")
     private TicketGroup parent;
 
     @OneToMany(mappedBy = "parent")
+    //TODO: cascade = CascadeType.ALL, fetch = FetchType.LAZY
     private List<TicketGroup> ticketGroupList;
 
 }
