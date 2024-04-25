68d73dd6d2b911beda1f5a95b5c101c0f7ae88e7
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-25 21:19:37 +0430

    BankTransaction Entity Cleaned - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/BankTransaction.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/BankTransaction.java
@@ -1,17 +1,19 @@
 package com.home.SpringBootAutomation.model;
 
+
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+@SuperBuilder
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@SuperBuilder
-@ToString
 
 @Entity(name = "BankTransactionEntity")
 @Table(name = "BankTransaction_tbl")
