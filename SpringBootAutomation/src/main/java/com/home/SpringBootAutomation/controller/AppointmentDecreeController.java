cfd2de039090fbca7ab059f028f21170632c229c
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-27 04:23:32 +0430

    Debugged - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/controller/AppointmentDecreeController.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/controller/AppointmentDecreeController.java
@@ -18,42 +18,42 @@ public class AppointmentDecreeController {
         this.serviceImp = serviceImp;
     }
 
-    @GetMapping(value = "/appointmentDecree")
-    public String showAppointmentDecreeList(Model model) {
-        model.addAttribute("appointmentDecree",new AppointmentDecree());
-        model.addAttribute("appointmentDecreeList", serviceImp.findAll());
-        return "appointmentDecree";
-    }
-
-    @PostMapping(value = "/appointmentDecree/save")
-    public String saveAppointmentDecree(AppointmentDecree appointmentDecree) {
-            serviceImp.save(appointmentDecree);
-            return "redirect:/appointmentDecree";
-    }
-
-    @GetMapping(value = "/edit/{id}")
-    public String showEditPage(@PathVariable("id") Long id, Model model) {
-        AppointmentDecree appointmentDecree = serviceImp.findById(id);
-        if (appointmentDecree != null) {
-            model.addAttribute("appointmentDecreeEdit", appointmentDecree);
-            return "appointmentDecreeEdit";
-        }
-        return "Invalid Id";
-    }
-
-    @PostMapping(value = "/update/{id}")
-    public String editForm(@PathVariable("id") Long id, @Valid AppointmentDecree appointmentDecree) {
-        serviceImp.save(appointmentDecree);
-        return "redirect:/appointmentDecree";
-    }
-
-    @GetMapping(value = "/delete/{id}")
-    public String deleteAppointmentDecree(@PathVariable("id") long id) {
-        AppointmentDecree appointmentDecree = serviceImp.findById(id);
-        if (appointmentDecree!=null) {
-            serviceImp.remove(appointmentDecree);
-            return "redirect:/appointmentDecree";
-        }
-        return "Invalid Id";
-    }
+//    @GetMapping(value = "/appointmentDecree")
+//    public String showAppointmentDecreeList(Model model) {
+//        model.addAttribute("appointmentDecree",new AppointmentDecree());
+//        model.addAttribute("appointmentDecreeList", serviceImp.findAll());
+//        return "appointmentDecree";
+//    }
+//
+//    @PostMapping(value = "/appointmentDecree/save")
+//    public String saveAppointmentDecree(AppointmentDecree appointmentDecree) {
+//            serviceImp.save(appointmentDecree);
+//            return "redirect:/appointmentDecree";
+//    }
+//
+//    @GetMapping(value = "/edit/{id}")
+//    public String showEditPage(@PathVariable("id") Long id, Model model) {
+//        AppointmentDecree appointmentDecree = serviceImp.findById(id);
+//        if (appointmentDecree != null) {
+//            model.addAttribute("appointmentDecreeEdit", appointmentDecree);
+//            return "appointmentDecreeEdit";
+//        }
+//        return "Invalid Id";
+//    }
+//
+//    @PostMapping(value = "/update/{id}")
+//    public String editForm(@PathVariable("id") Long id, @Valid AppointmentDecree appointmentDecree) {
+//        serviceImp.save(appointmentDecree);
+//        return "redirect:/appointmentDecree";
+//    }
+//
+//    @GetMapping(value = "/delete/{id}")
+//    public String deleteAppointmentDecree(@PathVariable("id") long id) {
+//        AppointmentDecree appointmentDecree = serviceImp.findById(id);
+//        if (appointmentDecree!=null) {
+//            serviceImp.remove(appointmentDecree);
+//            return "redirect:/appointmentDecree";
+//        }
+//        return "Invalid Id";
+//    }
 }
