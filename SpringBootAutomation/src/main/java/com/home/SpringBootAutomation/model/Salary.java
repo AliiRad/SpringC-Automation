6eaa6d03b0edb1025069a1521803e7bb46d2dfab
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-27 03:12:19 +0430

    TODO added to Salary - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Salary.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Salary.java
@@ -23,6 +23,8 @@ import java.io.Serializable;
 @Table(name = "salary_tbl")
 @ApplicationScope
 public class Salary implements Serializable {
+
+
     //جدول حقوق سالانه
     @Id
     @SequenceGenerator(name = "salarySeq" , sequenceName = "salary_seq", allocationSize = 1)
@@ -31,53 +33,56 @@ public class Salary implements Serializable {
     private Long id;
 
     //حقوق پایه ساعتی
+    //TODO: We Cannot Use @NotBlank Upon Integer values . maybe make nullable = false .
     @Column(name = "salary_basic_hourly_pay", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer basicHourlyPay;
 
     //حقوق پایه روزانه
+    //TODO: We Cannot Use @NotBlank Upon Integer values . maybe make nullable = false .
     @Column(name = "salary_basic_daily_pay", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer basicDailyPay;
 
     //حقوق پایه ماهانه
+    //TODO: We Cannot Use @NotBlank Upon Integer values . maybe make nullable = false .
     @Column(name = "salary_basic_monthly_pay", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer basicMonthlyPay;
 
     //بن کارگری
     @Column(name = "salary_working_coupon", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer workingCoupon;
 
     //حق مسکن
     @Column(name = "salary_housing_right", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer housingRight;
 
     //پایه سنوات
     @Column(name = "salary_working_year_pay", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer workingYearPay;
 
     //حق اولاد برای هر فرزند
     @Column(name = "salary_pay_for_each_child", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer payForEachChild;
 
     //حق عائله مندی افراد متاهل
     @Column(name = "salary_married_people_rights", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer marriedPeopleRights;
 
     //حق بیمه سهم کارگر
     @Column(name = "salary_insurance", length = 50)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     private Integer insurance;
 
     //TODO: max size for year
     @Column(name = "salary_year", length = 6, unique = true)
-    @NotBlank(message = "Should Not Be Null")
+//    @NotBlank(message = "Should Not Be Null")
     @Min(1400)
     private Integer year;
 
