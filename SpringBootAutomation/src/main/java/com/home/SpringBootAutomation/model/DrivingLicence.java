89b09231fb69917e5ba079c0434123dca447fe7f
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-27 01:26:24 +0430

    DrivingLicence Cleaned - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/DrivingLicence.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/DrivingLicence.java
@@ -18,50 +18,52 @@ import java.time.LocalDate;
 @Setter
 
 @Entity(name = "drivingLicenceEntity")
-@Table(name = "drivingLicence_Tbl")
+@Table(name = "driving_licence_tbl")
 public class DrivingLicence {
-//	//گواهینامه رانندگی
-
     @Id
     @SequenceGenerator(name = "drivingLicenceSeq", sequenceName = "drivingLicence_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "drivingLicenceSeq")
-    @Column(name = "drivingLicence_id")
+    @Column(name = "driving_licence_id")
     private Long id;
 
-    @Column(name = "drivingLicence_typeOfCert")
+    @Column(name = "driving_licence_type_of_cert")
     @NotBlank(message = "Should Not Be Null")
     @Enumerated(EnumType.ORDINAL)
     private TypeOfCertification typeOfCertification; //نوع گواهی نامه
 
-    @Column(name = "drivingLicence_issuanceDate")
+    @Column(name = "driving_licence_issuance_date")
     @PastOrPresent(message = "Invalid issuance Date")
     private LocalDate issuanceDate;//تاریخ صدور
 
-    @Column(name = "drivingLicence_serialNumber", length = 20)
+    @Column(name = "driving_licence_serial_number", columnDefinition = "NVARCHAR2(20)")
     @Pattern(regexp = "^[A-Za-zا-ی\\s]{1,20}$", message = "Invalid SerialNumber")
+    //TODO: Does it take character or only digit?
     @Size(min = 1, max = 20, message = "SerialNumber must be between 1 and 20 characters")
     @NotBlank(message = "Should Not Be Null")
     private String serialNumber; // شماره گواهی نامه
 
-    @Column(name = "drivingLicence_licenseSuspension", length = 30)
+    @Column(name = "driving_licence_license_suspension")
+    //TODO: Add @AssertFalse or @AssertTrue if needed.
     private boolean licenseSuspension;//تعلیق گواهینامه
 
-    @Column(name = "drivingLicence_licenseSuspensionDate")
+    @Column(name = "driving_licence_license_suspension_date")
     @PastOrPresent(message = "Invalid License Suspension Date")
     private LocalDate licenseSuspensionDate;//تاریخ تعلیق گواهینامه
 
-    @Column(name = "drivingLicence_endDate")
+    @Column(name = "driving_licence_end_date")
     @FutureOrPresent(message = "Invalid End Date")
     private LocalDate endDate;//تاریخ پایان اعتبار
 
-    @Column(name = "drivingLicence_renewal")
+    @Column(name = "driving_licence_renewal")
     @PastOrPresent(message = "Invalid Renewal Date")
     private LocalDate renewal;//تاریخ تمدید مجدد
 
+    @Column(name = "driving_licence_deleted")
+    private boolean deleted;//حذف
+
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "drivingLicence_personId")
+    @JoinColumn(name = "driving_licence_person_id")
     private Person person;
 
-    @Column(name = "drivingLicence_deleted")
-    private boolean deleted;//حذف
+
 }
