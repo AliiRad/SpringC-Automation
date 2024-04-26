b03ad01df6d48718afbf140d406b1cfdc3c64d20
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-27 00:01:27 +0430

    Added status code 409 - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/exceptions/DuplicateDataException.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/exceptions/DuplicateDataException.java
@@ -2,6 +2,7 @@ package com.home.SpringBootAutomation.exceptions;
 
 public class DuplicateDataException extends Exception{
 
+    /*409 is the correct status code for duplicate resource or resource already exists.*/
     private String message;
 
     public DuplicateDataException() {
@@ -12,4 +13,6 @@ public class DuplicateDataException extends Exception{
         super(message);
         this.message = message;
     }
+    public int responseStatus(){return 409;}
+
 }
