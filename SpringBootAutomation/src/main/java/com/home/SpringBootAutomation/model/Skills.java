4485480bf5b49bea6c79795e95dde6ac3cd78e07
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-27 03:13:26 +0430

    @NotBlank Removed From Skills - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Skills.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/Skills.java
@@ -34,7 +34,6 @@ public class Skills {
     private String skillTitle;
 
     @Column(name = "skill_rate" )
-    @NotBlank(message = "Should Not Be Null")
     @Enumerated(EnumType.ORDINAL)
     private SkillsGradeEn rate;
 
