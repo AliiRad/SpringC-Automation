749a000369e027a43df7a4299fa6e559c4df3a5e
mahursh <127805335+mahursh@users.noreply.github.com>
2024-04-28 06:00:41 +0430

    SkillsService Updated - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/SkillsService.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/SkillsService.java
@@ -2,31 +2,31 @@ package com.home.SpringBootAutomation.service;
 
 
 import com.home.SpringBootAutomation.enums.SkillsGradeEn;
+import com.home.SpringBootAutomation.exceptions.NoContentException;
 import com.home.SpringBootAutomation.model.Skills;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface SkillsService {
 
     Skills save(Skills skills);
-    Skills update(Long id , Skills skills);
+    Skills update(Skills skills) throws NoContentException;
 
     @Transactional
-    void logicalRemove(Long id);
+    void logicalRemove(Long id) throws NoContentException;
 
     List<Skills> findAll();
-    Skills findById(Long id);
+   Optional<Skills>findById(Long id)throws NoContentException;
     Long getSkillsCount();
 
-    Skills logicalRemoveWithReturn(Long id);
+    Skills logicalRemoveWithReturn(Long id) throws NoContentException;
 
     //-------------------------------------------------------------------
     //deletedFalse
 
     List<Skills> findSkillsByDeletedFalse();
-    Optional<Skills> findSkillsByIdAndDeletedFalse(Long id);
+    Optional<Skills> findSkillsByIdAndDeletedFalse(Long id) throws NoContentException;
 
 
     List<Skills> findSkillsBySkillTitleContainingIgnoreCaseAndDeletedFalse(String title);
--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/SkillsServiceImpl.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/SkillsServiceImpl.java
@@ -1,155 +1,138 @@
 package com.home.SpringBootAutomation.service.impl;
 
-
-
 import com.home.SpringBootAutomation.enums.SkillsGradeEn;
+import com.home.SpringBootAutomation.exceptions.NoContentException;
 import com.home.SpringBootAutomation.model.Skills;
 import com.home.SpringBootAutomation.repository.SkillsRepository;
 import com.home.SpringBootAutomation.service.SkillsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
-
 
 @Service
 public class SkillsServiceImpl implements SkillsService {
 
 
-    //------------------------------------------------------
-    // Constructor Injection
     private final SkillsRepository repository;
 
-    @Autowired
     public SkillsServiceImpl (SkillsRepository repository) {
         this.repository = repository;
     }
-
-    //------------------------------------------------------
 
 
     @Override
     public Skills save(Skills skills) {
         return repository.save(skills);
     }
-    //------------------------------------------------------
 
 
     @Override
-    public Skills update(Long id, Skills skills) {
-        Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(id);
+    public Skills update(Skills skills) throws NoContentException {
+        Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(skills.getId());
 
         if (optionalSkills.isPresent()){
-            Skills oldSkills = optionalSkills.get();
-            oldSkills.setSkillTitle(skills.getSkillTitle());
-            oldSkills.setRate(skills.getRate());
-            oldSkills.setTraining(skills.getTraining());
-            oldSkills.setDescription(skills.getDescription());
-            oldSkills.setCertification(skills.getCertification());
+//            Skills oldSkills = optionalSkills.get();
+//            oldSkills.setSkillTitle(skills.getSkillTitle());
+//            oldSkills.setRate(skills.getRate());
+//            oldSkills.setTraining(skills.getTraining());
+//            oldSkills.setDescription(skills.getDescription());
+//            oldSkills.setCertification(skills.getCertification());
 
 
-
-            return repository.save(oldSkills);
-        }else {
-            return null;
+            return repository.save(skills);
+        } else {
+            throw new NoContentException("Skill not found !");
         }
     }
-    //------------------------------------------------------
 
     @Transactional
     @Override
-    public void logicalRemove(Long id) {
+    public void logicalRemove(Long id) throws NoContentException {
         Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(id);
         if (optionalSkills.isPresent()){
             repository.logicalRemove(id);
         }else {
-            System.out.println(" Skill not found !");
+            throw new NoContentException("Skill not found !");
         }
-
-
     }
-    //------------------------------------------------------
 
 
     @Override
     public List<Skills> findAll() {
         return repository.findAll();
     }
-    //------------------------------------------------------
 
 
     @Override
-    public Skills findById(Long id) {
+    public Optional<Skills> findById(Long id) throws NoContentException {
         Optional<Skills> optional = repository.findById(id);
-        return optional.orElse(null);
+        if (optional.isPresent()) {
+            return optional;
+        } else {
+            throw new NoContentException("Skill not found !");
+        }
     }
-    //------------------------------------------------------
-
 
     @Override
     public Long getSkillsCount() {
         return repository.count();
     }
-    //------------------------------------------------------
 
 
     @Override
-    public Skills logicalRemoveWithReturn(Long id) {
+    public Skills logicalRemoveWithReturn(Long id) throws NoContentException {
         Optional<Skills> optionalSkills = repository.findSkillsByIdAndDeletedFalse(id);
+
         if (optionalSkills.isPresent()){
             Skills oldSkills = optionalSkills.get();
             oldSkills.setDeleted(true);
             return repository.save(oldSkills);
+
         } else {
-            return null;
+            throw new NoContentException("Skill not found !");
+
         }
     }
-    //------------------------------------------------------
-
 
     @Override
     public List<Skills> findSkillsByDeletedFalse() {
         return repository.findSkillsByDeletedFalse();
     }
-    //------------------------------------------------------
 
 
     @Override
-    public Optional<Skills> findSkillsByIdAndDeletedFalse(Long id) {
+    public Optional<Skills> findSkillsByIdAndDeletedFalse(Long id) throws NoContentException {
         Optional<Skills> optional = repository.findSkillsByIdAndDeletedFalse(id);
         if (optional.isPresent()){
             return optional;
-        }else return  Optional.empty();
+        }else {
+            throw new NoContentException("Skill not found !");
+
+        }
     }
-    //------------------------------------------------------
 
 
     @Override
     public List<Skills> findSkillsBySkillTitleContainingIgnoreCaseAndDeletedFalse(String title) {
         return repository.findSkillsBySkillTitleContainingIgnoreCaseAndDeletedFalse(title);
     }
-    //------------------------------------------------------
 
 
     @Override
     public List<Skills> findSkillsByRateAndDeletedFalse(SkillsGradeEn rate) {
         return repository.findSkillsByRateAndDeletedFalse(rate);
     }
-    //------------------------------------------------------
 
 
     @Override
     public List<Skills> findSkillsByTrainingContainingIgnoreCaseAndDeletedFalse(String training) {
         return repository.findSkillsByTrainingContainingIgnoreCaseAndDeletedFalse(training);
     }
-    //------------------------------------------------------
 
     @Override
     public Long countByDeletedFalse() {
         return repository.countByDeletedFalse();
     }
-    //------------------------------------------------------
 
 }
