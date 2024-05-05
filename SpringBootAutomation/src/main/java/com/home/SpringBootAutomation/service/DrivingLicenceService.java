0b078bf4843a4fae7eefddba0305e5f22c52c68c
Farhadghasemi <farhadghasemi.official@gmail.com>
2024-05-05 09:41:35 +0330

    DrivingLicenceService & DrivingLicenceServiceImpl Updated By farhad

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/DrivingLicenceService.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/DrivingLicenceService.java
@@ -1,14 +1,10 @@
 package com.home.SpringBootAutomation.service;
 
-
-
 import com.home.SpringBootAutomation.exceptions.NoContentException;
 import com.home.SpringBootAutomation.model.DrivingLicence;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.util.List;
-import java.util.Optional;
 
 
 public interface DrivingLicenceService {
@@ -17,14 +13,12 @@ public interface DrivingLicenceService {
 
     DrivingLicence update(DrivingLicence drivingLicence) throws NoContentException;
 
-    DrivingLicence remove(DrivingLicence drivingLicence) throws NoContentException;
-
     @Transactional
-    void logicalRemove(Long id) throws NoContentException;
+    DrivingLicence logicalRemove(Long id) throws NoContentException;
 
     List<DrivingLicence> findAll();
 
-    Optional<DrivingLicence> findByIdAndDeletedFalse(Long id) throws NoContentException;//chek shavad.
+    DrivingLicence findByIdAndDeletedFalse(Long id) throws NoContentException;
 
-    Optional<DrivingLicence> findById(Long id) throws NoContentException;
+    DrivingLicence findById(Long id) throws NoContentException;
 }
--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/DrivingLicenceServiceImpl.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/service/impl/DrivingLicenceServiceImpl.java
@@ -1,74 +1,50 @@
 package com.home.SpringBootAutomation.service.impl;
-
 import com.home.SpringBootAutomation.exceptions.NoContentException;
 import com.home.SpringBootAutomation.model.DrivingLicence;
-import com.home.SpringBootAutomation.model.Person;
 import com.home.SpringBootAutomation.repository.DrivingLicenceRepository;
-import com.home.SpringBootAutomation.repository.MilitaryRepository;
-import com.home.SpringBootAutomation.repository.PersonRepository;
 import com.home.SpringBootAutomation.service.DrivingLicenceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 @Slf4j
 public class DrivingLicenceServiceImpl implements DrivingLicenceService {
-    private final MilitaryRepository militaryRepository;
 
     private final DrivingLicenceRepository drivingLicenceRepository;
-    private final PersonRepository personRepository;
 
-    public DrivingLicenceServiceImpl(DrivingLicenceRepository drivingLicenceRepository, PersonRepository personRepository,
-                                     MilitaryRepository militaryRepository) {
+
+    public DrivingLicenceServiceImpl(DrivingLicenceRepository drivingLicenceRepository
+                                     ) {
         this.drivingLicenceRepository = drivingLicenceRepository;
-        this.personRepository = personRepository;
-        this.militaryRepository = militaryRepository;
+
     }
-
-
-
-
 
     @Override
     public DrivingLicence save(DrivingLicence drivingLicence) {
         log.info("Service-DrivingLicence-Save");
-        drivingLicenceRepository.save(drivingLicence);
-        return drivingLicence;
+        return drivingLicenceRepository.save(drivingLicence);
     }
 
     @Override
-    public DrivingLicence update(DrivingLicence drivingLicence)throws NoContentException {
+    public DrivingLicence update(DrivingLicence drivingLicence) throws NoContentException {
         log.info("Service-DrivingLicence-Update");
-        Optional<DrivingLicence>optionalDrivingLicence=drivingLicenceRepository.findByIdAndDeletedFalse(drivingLicence.getId());
-        if (optionalDrivingLicence.isPresent()){
-            return drivingLicenceRepository.save(drivingLicence);
-        }else {
-            throw new NoContentException("DrivingLicence not found !");
-        }
+        drivingLicenceRepository.findById(drivingLicence.getId()).orElseThrow(
+                () -> new NoContentException("No DrivingLicence Found with id : " + drivingLicence.getId())
+        );
+        return drivingLicenceRepository.save(drivingLicence);
     }
 
     @Override
-    public DrivingLicence remove(DrivingLicence drivingLicence) throws NoContentException {
-        log.info("Service-DrivingLicence-Remove");
-        if (findById(drivingLicence.getId()).isPresent()) {
-            drivingLicenceRepository.delete(drivingLicence);
-            return drivingLicence;
-        } else return null;
-    }
-
-    @Override
-    public void logicalRemove(Long id) throws NoContentException {
+    public DrivingLicence logicalRemove(Long id) throws NoContentException {
         log.info("Service-DrivingLicence-LogicalRemove");
-        Optional<DrivingLicence>optionalDrivingLicence=drivingLicenceRepository.findByIdAndDeletedFalse(id);
-        if (optionalDrivingLicence.isPresent()){
-            militaryRepository.logicalRemove(id);
-        }else {
-            throw new NoContentException("DrivingLicense not found !");
-        }
+        DrivingLicence drivingLicence = drivingLicenceRepository.findById(id).orElseThrow(
+                () -> new NoContentException("No DrivingLicence Found with id : " + id)
+        );
+        drivingLicenceRepository.logicalRemove(id);
+        return drivingLicence;
     }
 
     @Override
@@ -77,39 +53,21 @@ public class DrivingLicenceServiceImpl implements DrivingLicenceService {
         return drivingLicenceRepository.findAll();
     }
 
-
-
     @Override
-    public Optional<DrivingLicence> findByIdAndDeletedFalse(Long id) throws NoContentException {
+    public DrivingLicence findByIdAndDeletedFalse(Long id) throws NoContentException {
         log.info("Service-DrivingLicence-FindByIdAndDeletedFalse");
-        Optional<DrivingLicence> optionalDrivingLicence = drivingLicenceRepository.findByIdAndDeletedFalse(id);
-        if (optionalDrivingLicence.isPresent()){
-            return optionalDrivingLicence;
-        }else {
-            throw new NoContentException("DrivingLicence not found !");
-        }
+        return drivingLicenceRepository.findByIdAndDeletedFalse(id).orElseThrow(
+                () -> new NoContentException("No DrivingLicence Found with id : " + id)
+        );
     }
 
     @Override
-    public Optional<DrivingLicence> findById(Long id) throws NoContentException {
+    public DrivingLicence findById(Long id) throws NoContentException {
         log.info("Service-DrivingLicence-FindById");
-        Optional<DrivingLicence> optionalDrivingLicence = drivingLicenceRepository.findById(id);
-        if (optionalDrivingLicence.isPresent()){
-            return optionalDrivingLicence;
-        }else {
-            throw new NoContentException("DrivingLicence not found !");
-        }
+        return drivingLicenceRepository.findById(id).orElseThrow(
+                () -> new NoContentException("No DrivingLicence Found with id:" + id)
+        );
     }
 
-
-    public List<DrivingLicence> findByNationalId(String nationalId) throws NoContentException {
-        log.info("Service-DrivingLicence-findByNationalId : " + nationalId);
-        Optional<Person> person = personRepository.findPersonByNationalIdAndDeletedFalse(nationalId);
-        if (person.isPresent()) {
-            return person.get().getDrivingLicences();
-        } else {
-            throw new NoContentException("No Driving License");
-        }
-    }
 
 }
