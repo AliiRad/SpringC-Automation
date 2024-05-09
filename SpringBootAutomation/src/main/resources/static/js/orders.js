b565cf73e821d8e1d0298389e0efaaf9e28c3841
satyar <stking68@yahoo.com>
2024-05-09 23:33:27 +0330

    index.html Replaced with dashboard

---
+++ SpringBootAutomation/src/main/resources/static/js/orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'قابل پرداخت',
+        shipping:'در انتظار'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'پرداخت شد ',
+        shipping:'تحویل داده شد'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'پرداخت شد ',
+        shipping:'در انتظار'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'پرداخت شد ',
+        shipping:'تحویل داده شد'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'پرداخت شد ',
+        shipping:'رد شد'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'قابل پرداخت',
+        shipping:'تحویل داده شد'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'پرداخت شد ',
+        shipping:'رد شد'
+    },
+    {
+        productName: 'تست اول',
+        productNumber:'234567',
+        paymentStatus: 'قابل پرداخت',
+        shipping:'در انتظار'
+    }
+]
