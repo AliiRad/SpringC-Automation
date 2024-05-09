80952da290a532b7c2fc5929a79eca1e93ab9688
mahoor <mahurshams94@gmail.com>
2024-05-09 15:06:55 +0330

    TemplateException Added- Mahoor

---
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/exceptions/TemplateException.java
@@ -0,0 +1,18 @@
+package com.home.SpringBootAutomation.exceptions;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class TemplateException extends Exception {
+    private int statusCode;
+    private String statusText;
+
+    public TemplateException(String message)  {
+        super(message);
+        statusText = message;
+    }
+}
