60b5848f114b0b07d380aa640803feb8f684e084
mahoor <mahurshams94@gmail.com>
2024-05-09 12:28:15 +0330

    dashboard added - mahoor

---
+++ SpringBootAutomation/src/main/resources/templates/dashboard/en/js/enDashboard.js
@@ -0,0 +1,47 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+const langIcon = document.querySelector("#lang-icon")
+const langDropDown = document.querySelector("#lang-dropdown")
+
+
+menuBtn.addEventListener('click' , () => {
+    sideMenu.style.display = "block";
+})
+
+closeBtn.addEventListener("click" , () => {
+    sideMenu.style.display = "none";
+})
+
+themeToggler.addEventListener("click" , () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    // themeToggler.querySelector('span').classList.toggle('active');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle("active");
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle("active");
+})
+
+langIcon.addEventListener("click" , () =>{
+    langDropDown.classList.toggle("active")
+    setTimeout(()=>langDropDown.classList.remove("active"),5000)
+})
+
+
+OrdersEn.forEach(order => {
+    const tr=  document.createElement('tr');
+    const trContent =
+`
+    
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${order.shipping === 'Denied' ? 'danger' : order.shipping === 'Pending' ?'warning' : 'success'}">${order.shipping}</td>
+    <td class="primary">Details</td>
+    
+ `;
+
+ tr.innerHTML = trContent;
+ document.querySelector('table tbody').appendChild(tr);
+})
---
+++ SpringBootAutomation/src/main/resources/templates/dashboard/en/js/ordersEn.js
@@ -0,0 +1,51 @@
+const OrdersEn = [
+    {
+        productName: ' Test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: ' test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Denied'
+    },
+    {
+        productName: ' Test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: ' test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Denied'
+    },
+    {
+        productName: ' Test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: ' test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Denied'
+    },
+    {
+        productName: ' Test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: ' test',
+        productNumber:'234567',
+        paymentStatus: 'Due',
+        shipping:'Denied'
+    }
+    
+]
