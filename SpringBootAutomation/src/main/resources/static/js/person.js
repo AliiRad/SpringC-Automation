f507be4e580ea9df4179c401e3ed1d40302c0b03
Mahoor <mahurshams94@gmail.com>
2024-05-23 12:51:49 +0330

    Person Ui Added - Mahoor

--- SpringBootAutomation/src/main/resources/static/js/person.js
+++ SpringBootAutomation/src/main/resources/static/js/person.js
@@ -1,87 +1,51 @@
-// "Find Functionality"
-async function findById(id){
+// "Find By ID Functionality"
+async function findById(url,id){
+    console.log("find by id")
+
+    const response = await fetch(url + "/" + id,
+        {
+            method: "GET"
+        }
+    );
+    if (! response.ok){
+        showErrorPopup(url,response.status , (await response.text()).toString());
+    }else{
+        let person = JSON.parse(await response.text());
+
+        let editModal = document.getElementById("edit-modal");
+        editModal.style.display="flex";
+
+        let idEdit= document.querySelector("#edit-modal #id");
+        idEdit.value = person.id;
+
+        let nameEdit = document.querySelector("#edit-form div :nth-child(2)");
+        nameEdit.value =person.name;
+
+        let lastNameEdit = document.querySelector("#edit-form div :nth-child(5)");
+        lastNameEdit.value =person.lastname;
+
+        let fathersNameEdit = document.querySelector("#edit-form div :nth-child(8)");
+        fathersNameEdit.value =person.fathersName;
+
+        let certificateIDEdit = document.querySelector("#edit-form div :nth-child(11)");
+        certificateIDEdit.value =person.certificateID;
+
+        let nationalIDEdit = document.querySelector("#edit-form div :nth-child(14)");
+        nationalIDEdit.value =person.nationalID;
+
+        let birthdateEdit = document.querySelector("#edit-form div :nth-child(17)");
+        birthdateEdit.value =person.birthdate;
+
+        let genderEdit = document.querySelector("#edit-form div :nth-child(20)");
+        genderEdit.value =person.gender;
+
+        let marriageStatusEdit = document.querySelector("#edit-form div :nth-child(23)");
+        marriageStatusEdit.value =person.marriageStatus;
 
 
-    let editModal = document.getElementById("editModalPerson")
-    let modalPerson = new bootstrap.Modal(editModal);
-    modalPerson.show();
 
-    const resp = await fetch("/person/findById/" + id ,{
-        method :"GET"
-    });
-
-    const data = await resp.json();
-    let personId =document.getElementById("id__edit__person");
-    let personName = document.getElementById("firstName__edit__person");
-    let personFamily = document.getElementById("lastName__edit__person")
-    let personFather = document.getElementById("fathersName__edit__person");
-    let personUsername = document.getElementById("username__edit__person");
-    let personPassword = document.getElementById("password__edit__person");
-    let personCertificationID = document.getElementById("certificateID__edit__person");
-    let personNationalID = document.getElementById("nationalId__edit__person");
-    let personBirthdate = document.getElementById("birthdate__edit__person");
-    let personGender = document.getElementById("gender__edit__person");
-    let personMarriage = document.getElementById("marriageStatus__edit__person");
-
-
-    personId.value = data.id;
-    personName.value = data.name;
-    personFamily.value = data.lastname;
-    personFather.value =data.fathersName;
-    personUsername.value = data.username;
-    personPassword.value = data.password;
-    personCertificationID.value = data.certificateID;
-    personNationalID.value = data.nationalId;
-    personBirthdate.value = data.birthdate;
-    personGender.value = data.gender;
-    personMarriage.value = data.marriageStatus;
-
-
-}
-
-
-// "Edit Functionality"
-async function edit(){
-
-    const formData = new FormData(document.getElementById("editFormPerson"));
-
-    const resp = await fetch("/person/edit" ,{
-        method:"POST",
-        body: formData
-    });
-    // window.location.href = "/person"
-    // window.location.replace("/person")
-    // window.location.reload();
-    //
-    // if (resp.ok) {
-    //      console.log("ok")
-    //
-    // } else {
-    //     console.error("Failed to save edited person.");
-    // }
-
+    }
 
 }
 
 
-//"Delete Functionality"
-async function openDeleteModal(id){
-
-    let deleteIdInputPerson =document.getElementById("id__delete__person");
-
-    deleteIdInputPerson.value = id;
-
-    let deleteModal = document.getElementById("deleteModalPerson")
-    let modalPerson = new bootstrap.Modal(deleteModal);
-    modalPerson.show();
-
-
-}
-
-async function deleteById(id){
-    const resp= await fetch(`/person/delete/` + id,{
-        method:"DELETE"
-    });
-    console.log(id)
-    window.location.replace("/person")
-}
