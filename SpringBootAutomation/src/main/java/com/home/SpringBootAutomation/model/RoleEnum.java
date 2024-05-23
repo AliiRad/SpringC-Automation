824f01ed200d1c3101d9a021e3a5db05c7c0992d
Messbah.A <Messbah.A@Gmail.com>
2024-05-23 13:24:55 +0330

    updated

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/RoleEnum.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/RoleEnum.java
@@ -1,14 +1,14 @@
 package com.home.SpringBootAutomation.model;
 
-import org.springframework.security.core.GrantedAuthority;
-
-public enum RoleEnum implements GrantedAuthority {
+//import org.springframework.security.core.GrantedAuthority;
+//
+public enum RoleEnum { //} implements GrantedAuthority {
 
     ROLE_ADMIN,
     ROLE_USER;
 
-    @Override
-    public String getAuthority() {
-        return null;
-    }
+//    @Override
+//    public String getAuthority() {
+//        return null;
+//    }
 }
