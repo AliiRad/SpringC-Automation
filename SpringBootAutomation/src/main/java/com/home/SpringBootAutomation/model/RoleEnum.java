a09fd3ea3e5283f3bdf60770f450488049c37812
Mahoor <mahurshams94@gmail.com>
2024-05-23 11:36:00 +0330

    Role Enum Content Added - Mahoor

--- SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/RoleEnum.java
+++ SpringBootAutomation/src/main/java/com/home/SpringBootAutomation/model/RoleEnum.java
@@ -4,7 +4,8 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum RoleEnum implements GrantedAuthority {
 
-    ;
+    ROLE_ADMIN,
+    ROLE_USER;
 
     @Override
     public String getAuthority() {
